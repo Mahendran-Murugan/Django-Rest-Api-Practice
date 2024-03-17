c34e79955c75deae073246edbf78c747507f0372
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-03-17 14:35:54 +0530

    feat: add model for Advocate

--- base/models.py
+++ base/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+
+class Advocate(models.Model):
+    username = models.CharField(max_length = 100)
+    bio = models.TextField(max_length = 250, null = True, blank=True)
+    def __str__(self):
+        return self.username
+    
