2d7a7a29f1528a79f1f4bc7c605af19148ed3f8d
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-03-17 15:27:47 +0530

    feat: add query with Q

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.endpoint),
     path('advocates/', views.advocate_list),
+    path('add_advocate/', views.add_advocate),
     path('advocates/<str:username>/', views.advocate_details),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response 
+from django.db.models import Q
+from .models import Advocate
+from .serializers import AdvocateSerializer
 
 @api_view(['GET'])
 def endpoint(request):
@@ -8,10 +11,21 @@ def endpoint(request):
 
 @api_view(['GET'])
 def advocate_list(request):
-    data = ['Mahi', 'Kumar', 'Raju']
-    return Response(data)
+    query = request.GET.get('query')
+    if query == None:
+        query = ''
+    advocates = Advocate.objects.filter(Q(username__icontains=query) | Q(bio__icontains = query))
+    serializer = AdvocateSerializer(advocates, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def add_advocate(request):
+    Advocate.objects.create(
+        username = request.data['username']
+    )
 
 @api_view(['GET'])
 def advocate_details(request, username):
-    data = username
-    return Response(data)
+    advocate = Advocate.objects.get(username = username)
+    serializer = AdvocateSerializer(advocate, many=False)
+    return Response(serializer.data)
